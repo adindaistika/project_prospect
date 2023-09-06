dec74f935f6b3f96437f02fd759874a90edcdf39
adinda <adindaistka23@gmail.com>
2023-09-06 12:28:54 +0700

    _apps

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,21 @@
 import '@/styles/globals.css'
 import '@/styles/globals.scss'
+import { useRouter } from 'next/dist/client/router';
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter()
 
     return (
         <div className='min-h-screen'>
-            <div className='ml-[200px]'>
-                <div className='p-[20px] pt-[100px]'><Component {...pageProps} /></div>
-            </div>
+            {router.pathname === '/login' ?
+                <div className=''>
+                    <div className='p-[20px] pt-[100px]'><Component {...pageProps} /></div>
+                </div>
+                :
+                <div className='ml-[200px]'>
+                    <div className='p-[20px] pt-[100px]'><Component {...pageProps} /></div>
+                </div>
+            }
         </div>
     )
 
